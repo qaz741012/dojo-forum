e9986c8255c1c72509b25260c81905d52dfc69be
Chia-Hung Lin <qaz741012@gmail.com>
2018-04-28 23:19:21 +0800

    set m-m relationship between category and post through post_category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :post_categories, dependent: :restrict_with_error
+  has_many :posts, through: :post_categories
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, :content, :draft?, :user_id, presence: true
 
   has_many :replies, dependent: :destroy
   has_many :replied_users, through: :replies, source: :user
+
+  has_many :post_categories, dependent: :destroy
+  has_many :categories, through: :post_categories
 end
