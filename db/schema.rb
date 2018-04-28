8039c10172e8ae262fefd95297535d522cc02e30
Chia-Hung Lin <qaz741012@gmail.com>
2018-04-28 14:52:40 +0800

    generate Category and Post_category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_064443) do
+ActiveRecord::Schema.define(version: 2018_04_28_065132) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
