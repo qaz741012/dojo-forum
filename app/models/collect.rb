ee4604c99d36e2b827b7637f284957f204dc0fc6
Chia-Hung Lin <qaz741012@gmail.com>
2018-04-28 22:40:18 +0800

    set m-m relationship between user and post through collect

--- app/models/collect.rb
+++ app/models/collect.rb
@@ -1,2 +1,7 @@
 class Collect < ApplicationRecord
+  # validates :post_id, uniqueness: { scope: :user_id }
+  # validates :user_id, :post_id, presence: true
+
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :collects, dependent: :destroy
+  has_many :collected_posts, through: :collects, source: :post
+
 end
