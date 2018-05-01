9d12a677e8a9b5c530f00e9b7d3a9c11b5c128d3
Chia-Hung Lin <qaz741012@gmail.com>
2018-05-02 00:44:07 +0800

    user can create new post(but without category)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,23 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
     @categories = Category.all
   end
 
   def create
-    #code
+    render json: params
+    # add_draft = post_params
+    # add_draft[:draft?] = (params[:commit] == "Save Draft")
+    # post = current_user.posts.build(add_draft)
+    # if post.save
+    #
+    #
+    #   flash[:notice] = "The post was successfully created!"
+    #   redirect_to root_path
+    # else
+    #   flash[:alert] = post.errors.full_messages.to_sentence
+    #   redirect_back(fallback_location: root_path)
+    # end
   end
 
   def edit
@@ -21,4 +32,16 @@ class PostsController < ApplicationController
   def update
     #code
   end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :photo, :auth)
+  end
+
+  def post_category_params
+    params.require(:post_category).permit(:category_id)
+  end
+
 end
