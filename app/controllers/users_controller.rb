e3c0a4a8bf93ed42e0246ef6a3329469e729b794
Chia-Hung Lin <qaz741012@gmail.com>
2018-05-02 18:06:39 +0800

    user can only edit own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,22 @@ class UsersController < ApplicationController
     @drafts = @user.posts.where(draft?: true)
   end
 
-  def update
-    if @user.update(user_params)
-      flash[:notice] = "Profile was successfully updated."
-      redirect_to user_path(@user)
-    else
-      flash[:alert] = @user.errors.full_messages.to_sentence
+  def edit
+    if @user != current_user
+      flash[:alert] = "You are not allow to edit profile of the other users"
       redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def update
+    if @user == current_user
+      if @user.update(user_params)
+        flash[:notice] = "Profile was successfully updated."
+        redirect_to user_path(@user)
+      else
+        flash[:alert] = @user.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end
     end
   end
 
