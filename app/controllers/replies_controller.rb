9ffbf22cce1bb920fa8d9a967c24e3d4f2a01cc4
Chia-Hung Lin <qaz741012@gmail.com>
2018-05-08 22:52:15 +0800

    user can only reply on the post to which user have the authority

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -5,11 +5,15 @@ class RepliesController < ApplicationController
     reply = post.replies.build(reply_params)
     reply.user_id = current_user.id
 
-    if reply.save
-      post.update(last_replied_time: reply.created_at)
-      flash[:notice] = "Comment was successfully replied."
+    if post.auth_user?(current_user)
+      if reply.save
+        post.update(last_replied_time: reply.created_at)
+        flash[:notice] = "Comment was successfully replied."
+      else
+        flash[:alert] = reply.errors.full_messages.to_sentence
+      end
     else
-      flash[:alert] = reply.errors.full_messages.to_sentence
+      flash[:alert] = "You don't have authority to this post."
     end
     redirect_back(fallback_location: root_path)
   end
@@ -25,27 +29,31 @@ class RepliesController < ApplicationController
   def update
     post = Post.find(params[:post_id])
     reply = Reply.find(params[:id])
-    if reply.update(reply_params)
-      post.update(last_replied_time: reply.updated_at)
-      flash[:notice] = "Comment was successfully updated."
-      redirect_to post_path(reply.post)
+
+    if post.auth_user?(current_user)
+      if reply.update(reply_params)
+        post.update(last_replied_time: reply.updated_at)
+        flash[:notice] = "Comment was successfully updated."
+        redirect_to post_path(reply.post)
+      else
+        flash[:alert] = reply.errors.full_messages.to_sentence
+        redirect_back(fallback_location: root_path)
+      end
     else
-      flash[:alert] = reply.errors.full_messages.to_sentence
+      flash[:alert] = "You don't have authority to this post."
       redirect_back(fallback_location: root_path)
     end
-
   end
 
   def destroy
     reply = Reply.find(params[:id])
     if current_user != reply.user
       flash[:alert] = "You don't have authority to this post."
-      redirect_back(fallback_location: root_path)
     else
       reply.destroy
       flash[:notice] = "Comment was successfully deleted."
-      redirect_back(fallback_location: root_path)
     end
+    redirect_back(fallback_location: root_path)
   end
 
   private
