7906c9d0f025767b08259dde29303895f82c746d
Chia-Hung Lin <qaz741012@gmail.com>
2018-05-10 19:03:26 +0800

    finished create, update, and destroy action of API

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -16,4 +16,46 @@ class Api::V1::PostsController < ApiController
     end
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: {
+        message: "Post was successfully created.",
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if @post.update(post_params)
+      render json: {
+        message: "Post was successfully updated.",
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    render json: {
+      message: "Post was successfully deleted."
+    }
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content, :photo, :auth, :draft?)
+  end
+
 end
